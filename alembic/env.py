a6278ea77cd5cc7e4221df3573feb054cddba4fd
dedovk <ychuck33@gmail.com>
2026-05-04 17:19:42 +0300

    add initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -9,21 +9,30 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import pool
 from alembic import context
 
+# Додаємо шлях до проекту
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 config = context.config
-
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
 
-def run_migrations_offline() -> None:
-    db_url = str(settings.DATABASE_URL)
+def do_run_migrations(connection):
     context.configure(
-        url=db_url,
+        connection=connection,
+        target_metadata=target_metadata,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_offline() -> None:
+    url = str(settings.DATABASE_URL)
+    context.configure(
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -35,21 +44,14 @@ def run_migrations_offline() -> None:
 async def run_migrations_online() -> None:
     db_url = str(settings.DATABASE_URL)
 
-    print(f"====== DEBUG DATABASE URL: {db_url} ======")
-
     connectable = create_async_engine(
         db_url,
         poolclass=pool.NullPool,
     )
 
     async with connectable.connect() as connection:
-        await connection.run_sync(
-            lambda conn: context.configure(
-                connection=conn,
-                target_metadata=target_metadata,
-            )
-        )
-        await connection.run_sync(lambda conn: context.run_migrations())
+        # Виконуємо міграції через допоміжну функцію do_run_migrations
+        await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
 
