5198b4514efc2f6eb330f77cda717fa84fc02e77
dedovk <ychuck33@gmail.com>
2026-05-04 14:59:21 +0300

    modified db.py

--- AlcoLog/database/db.py
+++ AlcoLog/database/db.py
@@ -32,7 +32,7 @@ AsyncSessionLocal = async_sessionmaker(
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all, checkfirst=True)
 
 
 async def get_session():
